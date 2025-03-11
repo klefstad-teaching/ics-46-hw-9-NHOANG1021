586da34993568c6d379b1272ad0a8a74660dd1f0
Nicholas Hoang <nchoang1@uci.edu>
2025-03-11 00:15:43 +0000

    testing?

--- src/dijkstras.cpp
+++ src/dijkstras.cpp
@@ -39,8 +39,7 @@ vector<int> extract_shortest_path(const vector<int>& /*distances*/, const vector
 
 void print_path(const vector<int>& v, int total) {
     for (size_t i = 0; i < v.size(); ++i) {
-        if (i > 0) cout << " ";
-        cout << v[i];
+        cout << v[i] << " ";
     }
     // cout << " ";
     cout << "\nTotal cost is " << total << "\n";
